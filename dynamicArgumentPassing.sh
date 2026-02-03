7188085a6d3063594a20f601597340998d444bff
shabbani <shaz786shaiks@gmail.com>
2026-02-04 00:52:45 +0530

    dynamic variable

--- dynamicArgumentPassing.sh
+++ dynamicArgumentPassing.sh
@@ -2,8 +2,20 @@
 
 echo "Hello, World!"
 
+#$1 is used to dynamically pass arguments to the script.
+
 PERSON1=$1
 PERSON2=$2    
 
 echo "$PERSON1 : Welcome to Shell Scripting"
 echo "$PERSON2 : Thanks for the welcome message"
+
+#ask the user to provide username and password
+
+echo "Enter the Username"
+read USER_NAME
+
+echo "Username is: $USER_ NAME"
+
+echo "Enter the Password"
+read -s PASSWORD
