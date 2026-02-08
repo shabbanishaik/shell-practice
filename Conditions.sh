83e8b88073c328e6ff1facb1c09a564af650d4c6
shabbani <shaz786shaiks@gmail.com>
2026-02-08 10:28:41 +0530

    conditions changes3

--- Conditions.sh
+++ Conditions.sh
@@ -4,11 +4,17 @@ Numbers=$1
 
 if [ Nuumbers -gt 20]
 
+then
+
 echo "the no. is greater then 20"
 
 elif [Numbers -eq 20]
+
+then
 
 echo "the no. is equal to 20"
 
 else
 echo "the no. is less than 20"
+
+fi
