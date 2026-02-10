4c278e5855846b12d457cd671e7bdbc8187f44b3
shabbani <shaz786shaiks@gmail.com>
2026-02-11 00:57:33 +0530

    funtions commit

---
+++ functions.sh
@@ -0,0 +1,30 @@
+ 
+  #!/bin/bash
+
+  userId=$(id -u)  # This is to check the root user id or not .the root user id is 0
+
+if [ $userId -ne 0 ]; then
+
+  echo "Please run the script as root user"
+  exit 1
+
+
+  validate(){
+    if [ $1 -ne 0];
+    then
+      echo  "$2..... Installation failed"
+        exit 1
+
+        else
+
+         echo "$2..... Installation successful"
+
+
+
+  }
+   
+   dnf install nginx -y
+   validate $? "Installing nginx Web Server"
+
+fi
+  
