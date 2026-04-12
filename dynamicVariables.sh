08170be32f18e1ee8942c021c7655f7418af2148
shabbani <shaz786shaiks@gmail.com>
2026-04-13 01:28:04 +0530

    duynamic vars

---
+++ dynamicVariables.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+
+Start_Time=$(data+%s)
+echo "The Start Time is $Start_Time"
+
+End_Time=$(date +%s)
+echo "The End Time is $End_Time"
+
+Elapsed_Time=$((End_Time - Start_Time))
+echo "The Elapsed Time is $Elapsed_Time seconds"
