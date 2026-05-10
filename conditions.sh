dfb4f73538ce62d417a9a0c4168f05a3dcf11799
shabbani <shaz786shaiks@gmail.com>
2026-05-10 18:18:50 +0530

    new changes

--- conditions.sh
+++ conditions.sh
@@ -2,11 +2,32 @@
 #!/bin/bash
 
 
-validateUser= $1;
+# validateUser=$(id -u)
+# echo "the user id is $validateUser "
 
-if  [ "$validateUser" -eq $1 ]; then 
+# if  [ "$validateUser" -ne 0 ]; then 
 
-   echo "this is an root user"
-else
-    echo "this is not an root user"
-    fi
+#    echo "this is an root user"
+# else
+#    echo "this is not an root user"
+#     fi
+
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "This is not a root user."
+    exit 1
+ fi
+
+ echo "Installing Nginx web"
+ dnf install nginx y
+
+ if [$? -ne 0 ]; then
+    echo "Failed to install Nginx web server."
+    exit 1
+ 
+ else 
+     echo "Nginx web server installed successfully."
+ fi
+
+ 
